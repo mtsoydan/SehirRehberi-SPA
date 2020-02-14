5bb65a9e7a62b12a7030da8053f7e6e97020f0f4
mtsoydan <mustafataha.soydan@gmail.com>
2020-02-14 23:44:59 +0300

    Route -1

---
+++ src/app/routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { CityComponent } from './city/city.component';
+
+
+export const appRoutes:Routes =[
+    {path : "city" ,component :CityComponent},
+]
